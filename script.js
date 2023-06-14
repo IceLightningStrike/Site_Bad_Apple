1c0ebc8daf42cd390b36cc0d9eea040d83883235
unknown <german.542@gmail.com>
2023-06-15 00:40:37 +0300

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+var FrameNumber = 0;
+var FramesCount = 6574;
+
+
+function start_interval_reading(milliseconds) {
+    intervalReader = setInterval(FileReading, milliseconds); 
+}
+
+
+function FileReading() {
+    FrameNumber = (FrameNumber % FramesCount) + 1;
+
+    $.get(`data_${Math.floor(FrameNumber / 1000) + 1}/frame_${FrameNumber}.txt`, function(data) {
+        document.getElementById("demonstration").innerHTML = data;
+    });
+}
